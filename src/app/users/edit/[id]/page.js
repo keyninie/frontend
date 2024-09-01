99d2253ff4353d245161c1a8d468d86338d67571
keynine1 <vrzo5170@gmail.com>
2024-09-01 16:28:54 +0700

    upd

--- src/app/users/edit/[id]/page.js
+++ src/app/users/edit/[id]/page.js
@@ -1,25 +1,25 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-export default function Page({params}) {
-    const {id} = params;
+export default function Page({ params }) {
+  const { id } = params;
 
-    const [items, setItems] = useState([]);
-    useEffect(() => {
-      async function getUsers() {
-        try {
-          const res = await fetch(`http://localhost:3000/api/users/${id}`);
-          if (!res.ok) {
-            console.error('Failed to fetch data');
-            return;
-          }
-          const data = await res.json();
-          setItems(data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const res = await fetch(`https://backend-self-beta.vercel.app/api/users/${id}`);
+        if (!res.ok) {
+          console.error('Failed to fetch data');
+          return;
         }
+        const data = await res.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-   
+    }
+
     getUsers()
     //const interval  = setInterval(getUsers, 1000);
     //return () => clearInterval(interval);
@@ -35,12 +35,12 @@ export default function Page({params}) {
   const handleUpdateSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch('http://localhost:3000/api/users', {
+    const res = await fetch('https://backend-self-beta.vercel.app/api/users', {
       method: 'PUT',
       headers: {
-        Accept : 'application/json',
+        Accept: 'application/json',
       },
-      body: JSON.stringify({firstname, lastname, username, password, id}),
+      body: JSON.stringify({ firstname, lastname, username, password, id }),
     });
 
     const result = await res.json();
@@ -49,55 +49,55 @@ export default function Page({params}) {
 
   return (
     <>
-    <br /><br /><br />
-    <div className="container">
-    <div class="card">
-  <div class="card-header bg-success text-white">
+      <br /><br /><br />
+      <div className="container">
+        <div class="card">
+          <div class="card-header bg-success text-white">
             Edit Form
             {/* {JSON.stringify(items)} */}
-  </div>
-  <div class="card-body">
+          </div>
+          <div class="card-body">
 
-  {items.map((item) => (
-  <form className="row g-3" onSubmit={handleUpdateSubmit}>
-  <div className="col-md-6">
-    <label for="basic-url" className="form-label">FirstName</label>
-    <div className="input-group">
-      <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard"></i></span>
-      <input type="text" className="form-control" defaultValue={item.firstname} onChange={(e) => setFirstName(e.target.value)} required />
-    </div>
-  </div>
-  <div className="col-md-6">
-  <label for="basic-url" className="form-label">LastName</label>
-    <div className="input-group">
-      <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard-fill"></i></span>
-      <input type="text" className="form-control" defaultValue={item.lastname} onChange={(e) => setLastName(e.target.value)} required />
-    </div>
-  </div>
-  <div className="col-md-6">
-    <label for="basic-url" className="form-label">Username</label>
-    <div className="input-group">
-      <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard"></i></span>
-      <input type="text" className="form-control" defaultValue={item.username} onChange={(e) => setUserName(e.target.value)} required />
-    </div>
-  </div>
-  <div className="col-md-6">
-  <label for="basic-url" className="form-label">Password</label>
-    <div className="input-group">
-      <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard-fill"></i></span>
-      <input type="password" className="form-control" defaultValue={item.password} onChange={(e) => setPassWord(e.target.value)} required />
-    </div>
-  </div>
-  <div className="col-12">
-    <button type="submit" className="btn btn-success"><i class="bi bi-box-arrow-right"></i> Update</button>
-  </div>
-            </form>
-    ))}
-</div>
-</div>
-</div>
+            {items.map((item) => (
+              <form className="row g-3" onSubmit={handleUpdateSubmit}>
+                <div className="col-md-6">
+                  <label for="basic-url" className="form-label">FirstName</label>
+                  <div className="input-group">
+                    <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard"></i></span>
+                    <input type="text" className="form-control" defaultValue={item.firstname} onChange={(e) => setFirstName(e.target.value)} required />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <label for="basic-url" className="form-label">LastName</label>
+                  <div className="input-group">
+                    <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard-fill"></i></span>
+                    <input type="text" className="form-control" defaultValue={item.lastname} onChange={(e) => setLastName(e.target.value)} required />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <label for="basic-url" className="form-label">Username</label>
+                  <div className="input-group">
+                    <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard"></i></span>
+                    <input type="text" className="form-control" defaultValue={item.username} onChange={(e) => setUserName(e.target.value)} required />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <label for="basic-url" className="form-label">Password</label>
+                  <div className="input-group">
+                    <span className="input-group-text" id="basic-addon3"><i className="bi bi-person-vcard-fill"></i></span>
+                    <input type="password" className="form-control" defaultValue={item.password} onChange={(e) => setPassWord(e.target.value)} required />
+                  </div>
+                </div>
+                <div className="col-12">
+                  <button type="submit" className="btn btn-success"><i class="bi bi-box-arrow-right"></i> Update</button>
+                </div>
+              </form>
+            ))}
+          </div>
+        </div>
+      </div>
 
     </>
-    
+
   );
 }
